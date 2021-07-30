fa33e8adb38bdb0247347e387209fa348a1bbf62
Serhiy Koziuk <serhiy.koziuk@eleks.com>
2021-07-30 19:59:04 +0300

    added gulp file

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+const { src, dest, series, watch, parallel } = require('gulp');
+const connect = require('gulp-connect');
+const del = require('del');
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass')(require('sass'));;
+const autoprefixer = require('gulp-autoprefixer');
+
+const paths = {
+  src: __dirname + '/src/',
+  dist: __dirname + '/dist/'
+};
+
+function connectServer(cb) {
+  connect.server({
+    port: 8080,
+    root: paths.dist,
+    livereload: true
+  });
+
+  cb();
+}
+
+function clean() {
+  return del(paths.dist + '**', { fors: true });
+}
+
+function html() {
+  return src(paths.src + 'index.html')
+    .pipe(connect.reload())
+    .pipe(dest('./dist'));
+}
+
+function styles() {
+  return src(paths.src + 'styles/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(autoprefixer({
+      overrideBrowserslist: [
+        'last 2 version'
+      ],
+      cascade: false
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(connect.reload())
+    .pipe(dest('./dist'));
+}
+
+function watching(cb) {
+  watch('src/index.html', html);
+  watch('src/**/*.scss', styles);
+
+  cb();
+}
+
+const gulpTasks = [clean, html, styles];
+
+exports.build = series(...gulpTasks);
+exports.default = series(
+  ...gulpTasks,
+  parallel(connectServer, watching)
+);
